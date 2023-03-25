b2667119c4e1099a3311891833d2f046d722877b
Serenity <34622204+oSerenity@users.noreply.github.com>
2023-03-25 01:27:02 -0400

    gitignore

--- Fallout Shelter Save Editor/Form1.Designer.cs
+++ Fallout Shelter Save Editor/Form1.Designer.cs
@@ -38,7 +38,7 @@
             // 
             this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(98, 23);
+            this.button1.Size = new System.Drawing.Size(134, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "Decrypt Save";
             this.button1.UseVisualStyleBackColor = true;
@@ -48,7 +48,7 @@
             // 
             this.button2.Location = new System.Drawing.Point(12, 41);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(98, 23);
+            this.button2.Size = new System.Drawing.Size(134, 23);
             this.button2.TabIndex = 1;
             this.button2.Text = "Encrypt Save";
             this.button2.UseVisualStyleBackColor = true;
@@ -56,9 +56,9 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(12, 115);
+            this.button3.Location = new System.Drawing.Point(12, 99);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(127, 23);
+            this.button3.Size = new System.Drawing.Size(134, 23);
             this.button3.TabIndex = 3;
             this.button3.Text = "Open Win10 Location";
             this.button3.UseVisualStyleBackColor = true;
@@ -66,9 +66,9 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(12, 86);
+            this.button4.Location = new System.Drawing.Point(12, 70);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(127, 23);
+            this.button4.Size = new System.Drawing.Size(134, 23);
             this.button4.TabIndex = 2;
             this.button4.Text = "Open Steam Location";
             this.button4.UseVisualStyleBackColor = true;
